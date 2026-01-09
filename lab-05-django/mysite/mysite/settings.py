95ff2a7fdeefc04258a0bd16ea2268f59fd89090
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2026-01-09 18:34:45 -0500

    add docker settings

--- lab-05-django/mysite/mysite/settings.py
+++ lab-05-django/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sz%w&u%6(y9)4o^3x812p0sgflykymvb^w0ke(q4vk^b@^35z!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','*']
 
 # Application definition
 
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
+    'blog',
 ]
 
 MIDDLEWARE = [
